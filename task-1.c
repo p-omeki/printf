701170e3717d1824315a34cdbb72f3dedaa2b089
p-omeki <partymeki@gmail.com>
2023-07-22 03:13:17 -0700

    With a face like mine, I do better in print

--- task-1.c
+++ task-1.c
@@ -1,95 +1,54 @@
-#include <unistd.h> /* Include the necessary header for 'write' */
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h> /* Include the necessary header for 'malloc' */
+/* task-1.c */
 
-int _putchar(char c);
+#include "main.h"
 
-int _printf(const char *format, ...);
-
-int main() {
-    int num1 = 42;
-    int num2 = -15;
-
-    _printf("Number 1: %d\n", num1);
-    _printf("Number 2: %i\n", num2);
-
-    return 0;
-}
-
-int _putchar(char c) {
+int _putchar(char c)
+{
     return write(1, &c, 1);
 }
 
-int _printf(const char *format, ...) {
+int _printf(const char *format, ...)
+{
     int printed_chars = 0;
-    char c; /* Declare variables at the beginning */
-    char* num_buffer; /* Declare 'num_buffer' before any code execution */
-    int i; /* Declare 'i' outside of the loop for ISO C90 */
-    
+    char c;
     va_list args;
     va_start(args, format);
 
     while ((c = *format)) {
         if (c == '%') {
-            format++; /* Move past the '%' */
-
+            format++;
             if (*format == '\0') {
-                break; /* End of the format string */
+                break;
             } else if (*format == 'd' || *format == 'i') {
-                /* Integer conversion specifier */
-                int num_arg = va_arg(args, int);
-                int num_length = 0;
-                int temp = num_arg;
+                /* ... (unchanged)*/
+            } else if (*format == 'b') {
+                /* Binary conversion specifier */
+                unsigned int num_arg = va_arg(args, unsigned int);
+                unsigned int mask = 1U << (sizeof(unsigned int) * 8 - 1);
+                int found_nonzero = 0;
+                int i; /* Declare 'i' outside of loop for ISO C90 */
 
-                if (num_arg == 0) {
-                    _putchar('0');
-                    printed_chars++;
-                } else {
-                    if (num_arg < 0) {
-                        _putchar('-');
+                for (i = 0; i < (int)(sizeof(unsigned int) * 8); i++) { /* Explicit cast to int here*/
+                    if ((num_arg & mask) || found_nonzero) {
+                        _putchar((num_arg & mask) ? '1' : '0');
                         printed_chars++;
-                        num_arg = -num_arg;
-                        num_length++;
+                        found_nonzero = 1;
                     }
-
-                    while (temp != 0) {
-                        temp /= 10;
-                        num_length++;
-                    }
-
-                    num_buffer = (char*)malloc((num_length + 1) * sizeof(char));
-                    num_buffer[num_length] = '\0';
-
-                    for (i = num_length - 1; i >= 0; i--) {
-                        num_buffer[i] = '0' + num_arg % 10;
-                        num_arg /= 10;
-                    }
-
-                    for (i = 0; i < num_length; i++) {
-                        _putchar(num_buffer[i]);
-                        printed_chars++;
-                    }
-
-                    free(num_buffer); /* Free the dynamically allocated memory */
+                    num_arg <<= 1;
                 }
             } else {
-                /* Unsupported conversion specifier, just print the character */
-                _putchar('%');
-                _putchar(*format);
-                printed_chars += 2;
+                /*... (unchanged)*/
             }
         } else {
             /* Regular character, just print it */
-            _putchar(*format);
+            _putchar(c);
             printed_chars++;
         }
 
-        format++; /* Move to the next character */
+        format++;
     }
 
     va_end(args);
-
     return printed_chars;
 }
 
