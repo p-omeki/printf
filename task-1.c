113731981ad48302f90c99144fe1cb09ac6ebd94
p-omeki <partymeki@gmail.com>
2023-07-23 07:12:34 -0700

    Education is when you read the fine print. Experience is what you get if you don't

--- task-1.c
+++ task-1.c
@@ -1,47 +1,97 @@
-/* task-1.c */
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
-#include "main.h"
-
+/**
+ * _putchar - Writes a character to the standard output.
+ * @c: The character to write.
+ *
+ * Return: On success, returns the number of characters written.
+ * On error, returns -1.
+ */
 int _putchar(char c)
 {
     return write(1, &c, 1);
 }
 
+/**
+ * _printf - Prints formatted output to the standard output.
+ * @format: The format string containing conversion specifiers.
+ *
+ * Return: The number of characters printed (excluding the null byte).
+ */
 int _printf(const char *format, ...)
 {
     int printed_chars = 0;
     char c;
+    char *num_buffer;
+    int i;
+
     va_list args;
     va_start(args, format);
 
-    while ((c = *format)) {
-        if (c == '%') {
+    while ((c = *format))
+    {
+        if (c == '%')
+        {
             format++;
-            if (*format == '\0') {
+            if (*format == '\0')
                 break;
-            } else if (*format == 'd' || *format == 'i') {
-                /* ... (unchanged)*/
-            } else if (*format == 'b') {
-                /* Binary conversion specifier */
-                unsigned int num_arg = va_arg(args, unsigned int);
-                unsigned int mask = 1U << (sizeof(unsigned int) * 8 - 1);
-                int found_nonzero = 0;
-                int i; /* Declare 'i' outside of loop for ISO C90 */
+            else if (*format == 'd' || *format == 'i')
+            {
+                int num_arg = va_arg(args, int);
+                int num_length = 0;
+                int temp = num_arg;
 
-                for (i = 0; i < (int)(sizeof(unsigned int) * 8); i++) { /* Explicit cast to int here*/
-                    if ((num_arg & mask) || found_nonzero) {
-                        _putchar((num_arg & mask) ? '1' : '0');
-                        printed_chars++;
-                        found_nonzero = 1;
-                    }
-                    num_arg <<= 1;
+                if (num_arg == 0)
+                {
+                    _putchar('0');
+                    printed_chars++;
                 }
-            } else {
-                /*... (unchanged)*/
+                else
+                {
+                    if (num_arg < 0)
+                    {
+                        _putchar('-');
+                        printed_chars++;
+                        num_arg = -num_arg;
+                        num_length++;
+                    }
+
+                    while (temp != 0)
+                    {
+                        temp /= 10;
+                        num_length++;
+                    }
+
+                    num_buffer = (char *)malloc((num_length + 1) * sizeof(char));
+                    num_buffer[num_length] = '\0';
+
+                    for (i = num_length - 1; i >= 0; i--)
+                    {
+                        num_buffer[i] = '0' + num_arg % 10;
+                        num_arg /= 10;
+                    }
+
+                    for (i = 0; i < num_length; i++)
+                    {
+                        _putchar(num_buffer[i]);
+                        printed_chars++;
+                    }
+
+                    free(num_buffer);
+                }
             }
-        } else {
-            /* Regular character, just print it */
-            _putchar(c);
+            else
+            {
+                _putchar('%');
+                _putchar(*format);
+                printed_chars += 2;
+            }
+        }
+        else
+        {
+            _putchar(*format);
             printed_chars++;
         }
 
@@ -49,6 +99,21 @@ int _printf(const char *format, ...)
     }
 
     va_end(args);
+
     return printed_chars;
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int num = 42;
+
+    _printf("Decimal representation: %d\n", num);
+
+    return 0;
 }
 
