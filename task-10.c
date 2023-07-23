982f062a8d23f048d98b86a107b3119a218045a0
p-omeki <partymeki@gmail.com>
2023-07-23 04:11:09 -0700

    Fixing

---
+++ task-10.c
@@ -0,0 +1,77 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+
+int _printf(const char *format, ...)
+{
+    int field_width = 0;
+    char buffer[1024];
+    int buffer_index = 0;
+
+    va_list args;
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            
+           
+	    int precision = -1; /* Initialize precision to -1 to detect if it's set*/
+
+            if (*format == '*')
+            {
+                field_width = va_arg(args, int);
+                format++;
+            }
+            else
+            {
+                while (*format >= '0' && *format <= '9')
+                {
+                    field_width = field_width * 10 + (*format - '0');
+                    format++;
+                }
+            }
+
+            if (*format == '.')
+            {
+                format++;
+
+                if (*format == '*')
+                {
+                    precision = va_arg(args, int);
+                    format++;
+                }
+                else
+                {
+                    precision = 0;
+                    while (*format >= '0' && *format <= '9')
+                    {
+                        precision = precision * 10 + (*format - '0');
+                        format++;
+                    }
+                }
+            }
+
+            /* Rest of the code...*/
+
+            format++;
+        }
+        else
+        {
+            buffer[buffer_index++] = *format;
+            format++;
+        }
+    }
+
+    va_end(args);
+
+    write(1, buffer, buffer_index);
+
+    return buffer_index;
+}
+
