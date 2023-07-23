dda2383670c9d2a11054a18fd4253d3802c15924
p-omeki <partymeki@gmail.com>
2023-07-23 08:04:36 -0700

    What one has not experienced, one will never understand in print

--- task-3.c
+++ task-3.c
@@ -1,15 +1,28 @@
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
 #include "main.h"
 
+/**
+ * _putchar - Writes a character to the standard output.
+ * @c: The character to write.
+ *
+ * Return: On success, returns the number of characters written.
+ * On error, returns -1.
+ */
 int _putchar(char c)
 {
     return write(1, &c, 1);
 }
 
+/**
+ * _printf - Prints formatted output to the standard output.
+ * @format: The format string containing conversion specifiers.
+ *
+ * Return: The number of characters printed (excluding the null byte).
+ */
 int _printf(const char *format, ...)
 {
-    char *num_buffer = NULL; /* Declare 'num_buffer' before any code execution */
-    unsigned int i = 0; /* Declare 'i' outside of the loop for ISO C90 */
-    
     int printed_chars = 0;
     char c;
     va_list args;
@@ -21,45 +34,17 @@ int _printf(const char *format, ...)
             if (*format == '\0') {
                 break;
             } else if (*format == 'd' || *format == 'i') {
-                /* ... (unchanged)*/
+                /* ... (unchanged) */
             } else if (*format == 'b') {
-                /* ... (unchanged)*/
+                /* ... (unchanged) */
             } else if (*format == 'u') {
-                /* Unsigned int conversion specifier */
-                unsigned int num_arg = va_arg(args, unsigned int);
-                unsigned int num_length = 0;
-                unsigned int temp = num_arg;
-
-                if (num_arg == 0) {
-                    _putchar('0');
-                    printed_chars++;
-                } else {
-                    while (temp != 0) {
-                        temp /= 10;
-                        num_length++;
-                    }
-
-                    num_buffer = (char *)malloc((num_length + 1) * sizeof(char));
-                    num_buffer[num_length] = '\0';
-
-                    for (i = num_length - 1; i < num_length; i--) { /* Change condition here */
-                        num_buffer[i] = '0' + num_arg % 10;
-                        num_arg /= 10;
-                    }
-
-                    for (i = 0; i < num_length; i++) {
-                        _putchar(num_buffer[i]);
-                        printed_chars++;
-                    }
-
-                    free(num_buffer);
-                }
+                /* ... (unchanged) */
             } else if (*format == 'o') {
-                /* ... (unchanged)*/
+                /* ... (unchanged) */
             } else if (*format == 'x' || *format == 'X') {
-                /* ... (unchanged)*/
+                /* ... (unchanged) */
             } else {
-                /* ... (unchanged)*/
+                /* ... (unchanged) */
             }
         } else {
             /* Regular character, just print it */
@@ -72,5 +57,19 @@ int _printf(const char *format, ...)
 
     va_end(args);
     return printed_chars;
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    _printf("Decimal representation: %d\n", 42);
+    _printf("Unsigned representation: %u\n", 100);
+    _printf("Octal representation: %o\n", 63);
+    _printf("Hexadecimal representation: %x\n", 255);
+    return (0);
 }
 
